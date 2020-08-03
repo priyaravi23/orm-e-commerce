be4a6ea9df40af8a1474a9a5a97a841f8c610c06
Priya Ravi <priyaravi23@gmail.com>
2020-08-03 15:53:55 -0700

    added routes for product model

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
     // find all products
     // be sure to include its associated Category and Tag data
+    Product.findAll()
+        .then(dbProductData => res.json(dbProductData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
     // find a single product by its `id`
     // be sure to include its associated Category and Tag data
+    Product.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbProductData => {
+            if (!dbProductData) {
+                res.status(404).json({ message: 'No product found with this id' });
+                return;
+            }
+            res.json(dbProductData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // create new product
@@ -91,6 +113,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     // delete one product by its `id` value
+    Product.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbProductData => {
+            if (!dbProductData) {
+                res.status(404).json({ message: 'No product found with this id' });
+                return;
+            }
+            res.json(dbProductData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
